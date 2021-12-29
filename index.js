96b4471d1b39e90c417530fe617d7f4bed718de0
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-12-28 22:33:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const client = new DiscordMusicBot();
 client.build();
 
 module.exports = client; //;-;
+
+var http = require("http");
+setInterval(function() {
+    http.get("https://viera-bot.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
